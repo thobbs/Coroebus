cb508ecba450f4792a24c4b42c661f86682e260a
Tyler Hobbs <tyler@riptano.com>
2010-12-09 17:54:35 -0600

    More ColumnFamily tests

--- tests/column_family_test.cpp
+++ tests/column_family_test.cpp
@@ -37,7 +37,7 @@ TEST(ColumnFamily, TestEmpty)
     ASSERT_EQ(results[key].size(), 0);
 }
 
-TEST(ColumnFamily, TestGet)
+TEST(ColumnFamily, Get)
 {
     Connection * connection = new Connection("Keyspace1", "localhost:9160");
     ColumnFamily * cf = new ColumnFamily(connection, "Standard1");
@@ -90,7 +90,7 @@ TEST(ColumnFamily, TestGet)
     cf->remove(key);
 }
 
-TEST(ColumnFamily, TestMultiget)
+TEST(ColumnFamily, Multiget)
 {
     Connection * connection = new Connection("Keyspace1", "localhost:9160");
     ColumnFamily * cf = new ColumnFamily(connection, "Standard1");
@@ -120,7 +120,104 @@ TEST(ColumnFamily, TestMultiget)
     ASSERT_EQ(results.size(), 2);
     ASSERT_TRUE(results == expected);
 
+    // specify a set of column names
+    vector<string> colnames;
+    colnames.push_back("1");
+    results = cf->multiget(keys, colnames);
+
+    expected = map<string, map<string, string> >();
+    map<string, string> expected_columns;
+    expected_columns.insert(pair<string, string>("1", "val1"));
+    expected.insert(pair<string, map<string, string> >(key1, expected_columns));
+    expected.insert(pair<string, map<string, string> >(key2, expected_columns));
+
+    ASSERT_EQ(results.size(), 2);
+    ASSERT_TRUE(results == expected);
+
+    // do a slice
+    results = cf->multiget(keys, "1", "", 1);
+    ASSERT_EQ(results.size(), 2);
+    ASSERT_TRUE(results == expected);
+
     // cleanup
     cf->remove(key1);
     cf->remove(key2);
+}
+
+TEST(ColumnFamily, GetCount)
+{
+    Connection * connection = new Connection("Keyspace1", "localhost:9160");
+    ColumnFamily * cf = new ColumnFamily(connection, "Standard1");
+    string key = "ColumnFamily.GetCount";
+
+    map<string, string> columns;
+    columns.insert(pair<string, string>("1", "val1"));
+    columns.insert(pair<string, string>("2", "val2"));
+    cf->insert(key, columns);
+
+    ASSERT_EQ(cf->get_count(key), 2);
+    ASSERT_EQ(cf->get_count(key, "", ""), 2);
+    ASSERT_EQ(cf->get_count(key, "1", "2"), 2);
+
+    vector<string> colnames;
+    colnames.push_back("1");
+    ASSERT_EQ(cf->get_count(key, colnames), 1);
+
+    // cleanup
+    cf->remove(key);
+}
+
+TEST(ColumnFamily, MultigetCount)
+{
+    Connection * connection = new Connection("Keyspace1", "localhost:9160");
+    ColumnFamily * cf = new ColumnFamily(connection, "Standard1");
+    string key1 = "ColumnFamily.MultigetCount1";
+    string key2 = "ColumnFamily.MultigetCount2";
+    string key3 = "ColumnFamily.MultigetCount3";
+
+    map<string, string> columns;
+    columns.insert(pair<string, string>("1", "val1"));
+    columns.insert(pair<string, string>("2", "val2"));
+    cf->insert(key1, columns);
+    cf->insert(key2, columns);
+    columns = map<string, string>();
+    columns.insert(pair<string, string>("1", "val1"));
+    cf->insert(key3, columns);
+
+    map<string, int32_t> expected;
+    expected.insert(pair<string, int32_t>(key1, 2));
+    expected.insert(pair<string, int32_t>(key2, 2));
+    expected.insert(pair<string, int32_t>(key3, 1));
+
+    vector<string> keys;
+    keys.push_back(key1);
+    keys.push_back(key2);
+    keys.push_back(key3);
+    ASSERT_TRUE(cf->multiget_count(keys) == expected);
+
+    // cleaup
+    cf->remove(key1);
+    cf->remove(key2);
+    cf->remove(key3);
+}
+
+TEST(ColumnFamily, Remove)
+{
+    Connection * connection = new Connection("Keyspace1", "localhost:9160");
+    ColumnFamily * cf = new ColumnFamily(connection, "Standard1");
+    string key = "ColumnFamily.Remove";
+
+    map<string, string> columns;
+    columns.insert(pair<string, string>("1", "val1"));
+    columns.insert(pair<string, string>("2", "val2"));
+    cf->insert(key, columns);
+    ASSERT_EQ(cf->get_count(key), 2);
+
+    vector<string> column_names;
+    column_names.push_back("1");
+    cf->remove(key, column_names);
+    ASSERT_EQ(cf->get_count(key), 1);
+
+    cf->remove(key);
+    ASSERT_EQ(cf->get_count(key), 0);
 }
